89f7bb60474406cac998c869a98ba871a043a590
Anthony Corletti <anthcor@gmail.com>
2022-02-19 18:57:33 -0500

    small: bump python and apply mypy on tests

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-mypy python_project_template
+mypy python_project_template tests
 flake8 python_project_template tests
 black python_project_template tests --check
 isort python_project_template tests scripts --check-only
