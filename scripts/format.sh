18643c87b0b5ea51ddfa64e328075eb8a6c819f8
Anthony Corletti <anthony@rungalileo.io>
2022-11-04 16:52:32 -0400

    feat: ruff

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -ex
 
-# Sort imports one per line, so autoflake can remove unused imports
-isort --force-single-line-imports python_project_template tests scripts
-
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place python_project_template tests scripts --exclude=__init__.py
 black python_project_template tests scripts
 isort python_project_template tests scripts
+ruff python_project_template tests scripts --fix
