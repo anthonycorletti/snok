db2dbdbf473da2b0f7bbaa58fb162465920e4afa
Anthony Corletti <anthcor@gmail.com>
2022-11-29 16:08:57 -0500

    chore: bump python, remove isort

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -ex
 
 black python_project_template tests scripts
-isort python_project_template tests scripts
 ruff python_project_template tests scripts --fix
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,5 +2,4 @@
 
 mypy python_project_template tests
 black python_project_template tests --check
-isort python_project_template tests scripts --check-only
 ruff python_project_template tests scripts
