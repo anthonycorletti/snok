7fbfc7afcef750509464f458c8ff8770ae5e551b
Anthony Corletti <anthcor@gmail.com>
2023-07-26 12:54:10 -0400

    wip

--- snok/templates/__app/_modal.py
+++ snok/templates/__app/_modal.py
@@ -6,8 +6,8 @@ from {{ __template_name }}.config import Settings
 stub = Stub(name="{{ __template_name }}")
 Settings.Config.env_file = ".env.prod"
 stub["env"] = Secret.from_dict(
-    {str(k): str(v) for k, v in Settings().dict().items()}
-)  # type: ignore
+    {str(k): str(v) for k, v in Settings().dict().items()}  # type: ignore
+)
 image = Image.debian_slim().pip_install_from_pyproject("pyproject.toml")
 
 
